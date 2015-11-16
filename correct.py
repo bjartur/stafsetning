8112e502de69f170f80d5fccce10fab96dd1a995
Árni Geir Úlfarsson <arnigeirulfarsson@htgi-dw0869.rhi.hi.is>
2015-11-16 20:52:39 +0000

    more cleaning

--- correct.py
+++ correct.py
@@ -18,7 +18,24 @@ def read_in_test_data(word_count, word_frequency, following_word):
 
     def count_seen_wordpair(previous_word, current_word):
         return following_word[previous_word].get(current_word) or 0
-	
+
+    def create_guess(prev_word, word):
+        if exists(word) and exists(prev_word) and (common(word) or common(prev_word)) and (rare(word) or rare(prev_word)):
+            guess = word
+        elif exists(prev_word) and count_seen_wordpair(prev_word, word) > 0:
+            guess = word
+        elif prev_guess != prev_word and count_seen_wordpair(prev_guess, word) > 0:
+            guess = word
+        else:
+            # We don't know if prev_word existed.
+            # So let's use prev_guess to be safe.
+            guess = best_guess(prev_guess, word)
+        if not prev_word:
+            # The first word in a sentence.
+            guess.capitalize()
+        return guess
+
+
     # Assuming previous_word exists
     def best_guess(previous_word, current_word):
         least_distance = max_word_change
@@ -36,31 +53,19 @@ def read_in_test_data(word_count, word_frequency, following_word):
 
     with open('althingi_errors/079.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
-
         prev_word = ""
         prev_guess = ""
         unnoticed_errors = 0
         wrong_guesses = 0
         false_errors = 0
         total_words = 0
+        before = time.process_time()
+
         for row in reader:
-            before = time.process_time()
             word = row['Word']
             if word == ",":
                 continue
-            if exists(word) and exists(prev_word) and (common(word) or common(prev_word)) and (rare(word) or rare(prev_word)):
-                guess = word
-            elif exists(prev_word) and count_seen_wordpair(prev_word, word) > 0:
-                guess = word
-            elif prev_guess != prev_word and count_seen_wordpair(prev_guess, word) > 0:
-                guess = word
-            else:
-                # We don't know if prev_word existed.
-				# So let's use prev_guess to be safe.
-                guess = best_guess(prev_guess, word)
-            if not prev_word:
-                # The first word in a sentence.
-                guess.capitalize()
+            guess = create_guess(prev_word, word)
             correct_word = row['CorrectWord']
             if guess != correct_word:
                 if word == guess:
--- spell.py
+++ spell.py
@@ -16,7 +16,7 @@ def missing(i):
         return False
 
 
-def create_dicts():
+def read_files():
     for i in range(79,81):
         if not missing(i):
             prefix = 'althingi_tagged/'
@@ -26,29 +26,34 @@ def create_dicts():
                 filename = prefix + str(i) + '.csv'
             with open(filename, newline='', encoding='utf-8') as csvfile:
                 reader = csv.DictReader(csvfile)
+                print("Creating dict from althingi tagged nr: ", i)
                 prev_word = ""
                 for row in reader:
-                    cur_word = row['Word']
-                    if cur_word == ",":
-                        continue
-                    if not prev_word:
-                        cur_word = cur_word.lower()
-                    if not word_frequency.get(cur_word):
-                        word_frequency[cur_word] = 1
-                    else:
-                        word_frequency[cur_word] += 1
-                    if not following_word.get(prev_word):
-                        following_word[prev_word] = {}
-                        following_word[prev_word][cur_word] = 1
-                    elif not following_word[prev_word].get(cur_word):
-                        following_word[prev_word][cur_word] = 1
-                    else:
-                        following_word[prev_word][cur_word] += 1
-                    prev_word = cur_word
-                    global word_count
-                    word_count += 1
+                    prev_word = create_dicts(prev_word, row['Word'])
 
-create_dicts()
+
+def create_dicts(prev_word, cur_word):
+    if cur_word == ",":
+        return prev_word
+    if not prev_word:
+        cur_word = cur_word.lower()
+    if not word_frequency.get(cur_word):
+        word_frequency[cur_word] = 1
+    else:
+        word_frequency[cur_word] += 1
+    if not following_word.get(prev_word):
+        following_word[prev_word] = {}
+        following_word[prev_word][cur_word] = 1
+    elif not following_word[prev_word].get(cur_word):
+        following_word[prev_word][cur_word] = 1
+    else:
+        following_word[prev_word][cur_word] += 1
+    prev_word = cur_word
+    global word_count
+    word_count += 1
+    return prev_word
+
+read_files()
 print("Press <Enter> to start correcting.")
 while input() == "":
     from correct import read_in_test_data
