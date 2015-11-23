4269830e343399957f5d16ec3a278ddd1e25f617
Bjartur Thorlacius <bjt9@hi.is>
2015-11-23 10:01:12 +0000

    Skoða fleiri ocr villur

--- parameters.py
+++ parameters.py
@@ -1,12 +1,12 @@
 # Put the name of your input file in line below
-filename = 'althingi_errors/079.csv' # <- OVER HERE!
+filename = 'althingi_errors/095.csv' # <- OVER HERE!
 
 
 max_change_context = 1
 max_change_optical = 2
 treshold_common = 0.005
 treshold_rare   = 0.001
-min_err_freq = 5
+min_err_freq = 2
 
 # File for training data for populating word_frequency and following_word
 training_data = "known_errors.csv"
